a459193bb34b6dc7c33065b8702e7970ba5cc6d4
Jakob Skindstad Frederiksen <jakobsfrederiksen1@gmail.com>
2024-11-12 16:53:16 +0100

    Save to code tomorrow

---
+++ QMYoga/wwwroot/index.js
@@ -0,0 +1,20 @@
+﻿
+function toggleContent(isVisible) {
+  const collapsibleContent = document.getElementById("collapsibleContent");
+    if (isVisible) {
+       collapsibleContent.style.display = "block";
+    } else {
+       collapsibleContent.style.display = "none";
+    }
+}
+
+function scrollCarousel(direction) {
+  const carouselContainer = document.getElementById('carouselContainer');
+  const imageWidth = 210; // Assuming fixed image width + margin
+  const imagesToShow = 5;
+  const scrollAmount = direction * imageWidth * imagesToShow;
+  carouselContainer.scrollBy({
+     left: scrollAmount,
+     behavior: 'smooth'
+  });
+}
