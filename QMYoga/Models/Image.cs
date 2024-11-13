5e3a16831674345e92cb0e19727b58dd3eef5d21
Skindstad <39044695+Skindstad@users.noreply.github.com>
2024-11-13 11:44:13 +0100

    Playlist Model

--- QMYoga/Models/Image.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QMYoga.Models
-{
-    public class Image
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Url { get; set; }
-    }
-}
